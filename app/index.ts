460b3b2f07a7fc3eefcee9823659562d4fd39ae5
Valentin Dide <vali.dide@gmail.com>
2024-04-19 23:16:11 +0300

    Test

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,10 @@
-alert("Application is running...");
+import { PubSubHub, delay } from "../src/index.ts";
+
+const pubSubHub = new PubSubHub();
+
+pubSubHub.subscribe("appStarted", (_data, message) => {
+  alert(`${message.appName} is running...`);
+});
+
+await delay(1_000);
+pubSubHub.publish("appStarted", { appName: "My App" });
