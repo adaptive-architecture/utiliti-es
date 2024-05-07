c2e728e74742ec655384c863bb72c7c89c6850ae
Valentin Dide <vali.dide@gmail.com>
2024-05-07 17:23:08 +0300

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/utiliti-es/",
   title: "@adapt-arch/utiliti-es",
   description: "ECMAScript common utilities library",
   themeConfig: {
